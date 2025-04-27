8728b25ad93a2e95e54540cb110263ca3e54dcf0
MohamedMando5 <mohamedemad22440@gmail.com>
2025-04-27 17:32:31 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import psycopg2
 import base64
+from io import BytesIO
+from PIL import Image
+import io
 
 app = Flask(__name__)
 CORS(app)
